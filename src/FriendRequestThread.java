38d38b3cc8dfb95e11cb87f8a5ed7cd03678d830
Aryaman Modi <aryamanmodi@Aryamans-MacBook-Air-2.local>
2025-11-15 01:05:40 -0600

    Added javadoc to friendrequestthread

--- src/FriendRequestThread.java
+++ src/FriendRequestThread.java
@@ -1,8 +1,22 @@
+/**
+ * A thread that simulates sending a friend request between two students
+ * in the Longhorn Network. Meant to be executed on multiple threads to simulate 
+ * concurrency.
+ */
 public class FriendRequestThread implements Runnable {
+    /**
+     * Creates a new {@code FriendRequestThread}
+     *
+     * @param sender   the student initiating the friend request
+     * @param receiver the student receiving the friend request
+     */
     public FriendRequestThread(UniversityStudent sender, UniversityStudent receiver) {
         // Constructor
     }
 
+    /**
+     * Executes the friend request simulation. Must be thread safe.
+     */
     @Override
     public void run() {
         // Method signature only
